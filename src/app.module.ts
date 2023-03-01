28e5f8efa01b5daea689ad6984de5d76ddea7d13
ken <ynkn0829@gmail.com>
2023-03-01 23:52:25 +0900

    yarn nest generate module components/posts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { PostsModule } from './components/posts/posts.module';
 import * as path from 'path';
 
 @Module({
@@ -8,6 +9,7 @@ import * as path from 'path';
       autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
     }),
+    PostsModule,
   ],
 })
 
