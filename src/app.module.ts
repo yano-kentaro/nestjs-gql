a81577d4cd71b5ae069ead86c4b9e7e19a4df2f9
ken <ynkn0829@gmail.com>
2023-03-08 21:42:59 +0900

    ConfigModuleをAppModuleへ結合

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
+
+import * as path from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import { PostsModule } from '@/posts/posts.module';
+
 import { AppController } from 'app.controller';
 import { AppService } from 'app.service';
-import { GraphQLModule } from '@nestjs/graphql';
-import * as path from 'path';
-import { PostsModule } from '@/posts/posts.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
+    }),
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local']
     }),
     PostsModule,
   ],
